fa867d5f2e6c996b62638c20acc2326261542ddc
rvcas <x@rvcas.dev>
2022-12-14 17:46:28 -0500

    feat: bring mdbook docs over

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx'
-})
+const { getHighlighter, BUNDLED_LANGUAGES } = require("shiki");
+
+const rehypePrettyCodeOptions = {
+  getHighlighter: (options) => {
+    return getHighlighter({
+      ...options,
+      langs: [
+        ...BUNDLED_LANGUAGES,
+        {
+          id: "aiken",
+          scopeName: "source.aiken",
+          grammar: require("./aiken.tmLanguage.json"),
+          aliases: ["ak", "aiken"],
+        },
+      ],
+    });
+  },
+};
+
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  mdxOptions: { rehypePrettyCodeOptions },
+});
 
 module.exports = withNextra();
